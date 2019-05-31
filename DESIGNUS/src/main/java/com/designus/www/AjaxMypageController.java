8cd6a2be5f0ee17c1b7b5cedd00b86de87aff1eb
wildmonk1025 <50566047+wildmonk1025@users.noreply.github.com>
2019-05-31 15:40:04 +0900

    클래스 테스트

--- DESIGNUS/src/main/java/com/designus/www/AjaxMypageController.java
+++ DESIGNUS/src/main/java/com/designus/www/AjaxMypageController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AjaxMypageController {
 	/*
-	 * @RequestMapping() public String norJoinFrm() {
-	 * 
-	 * return "norjoinFrm"; }
+	 * @RequestMapping() public String norJoinFrm() { return "norjoinFrm"; }
 	 */
+	 
 }
