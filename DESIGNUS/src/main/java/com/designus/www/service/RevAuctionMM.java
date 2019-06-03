27b6d4c24677179c6f97ba2cc5bcc57cc2ef9be3
wildmonk1025 <50566047+wildmonk1025@users.noreply.github.com>
2019-06-03 20:26:16 +0900

    변경자 : 조용환 변경사항 : auctioncontroller_sub

--- DESIGNUS/src/main/java/com/designus/www/service/RevAuctionMM.java
+++ DESIGNUS/src/main/java/com/designus/www/service/RevAuctionMM.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.designus.www.bean.RevAuction;
+
 
 
 public class RevAuctionMM {
@@ -28,17 +30,19 @@ public class RevAuctionMM {
 		String contents = multi.getParameter("b_contents");
 		int check = Integer.parseInt(multi.getParameter("fileCheck"));
 		
+		RevAuction ra = new RevAuction();
 		/*
-		 * board.setB_title(title); board.setB_contents(contents); board.setB_mid(id);
-		 * boolean b= bDao.boardInsert(board); //board.setB_num(bDao.getBoardNum());
-		 * //BoardFile 등록을 위해 DB에서 글번호 가져옴 boolean f = false; if(check==1) {
-		 * //UploadFile upload = new UploadFile(); //프로토타입 //서버에 파일을 업로드 한 후, //오리지널
-		 * 파일명, 시스템파일명을 리턴 후 맵에 저장 f = upload.fileUp(multi, board.getB_num()); }
+		 * ra.setB_title(title); ra.setB_contents(contents); ra.setB_mid(id); boolean b=
+		 * bDao.raInsert(ra); //ra.setB_num(bDao.getraNum()); //raFile 등록을 위해 DB에서 글번호
+		 * 가져옴 boolean f = false; if(check==1) { //UploadFile upload = new UploadFile();
+		 * //프로토타입 //서버에 파일을 업로드 한 후, //오리지널 파일명, 시스템파일명을 리턴 후 맵에 저장 f =
+		 * upload.fileUp(multi, ra.getB_num()); }
+		 * 
+		 * 
+		 * if(b && f) { //글쓰기 성공 view = "redirect:boardList"; } else { view ="writeFrm";
+		 * }
 		 */
-		/*
-		 * if(b && f) { //글쓰기 성공 view = "redirect:boardList"; } else { view =
-		 * "writeFrm"; }
-		 */
+		 
 		
 		mav.setViewName(view);
 		return mav;
