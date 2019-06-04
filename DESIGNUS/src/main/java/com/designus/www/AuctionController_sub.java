b56f42250ea6f726d8cccfcd238ec4adef9fc53f
wildmonk1025 <50566047+wildmonk1025@users.noreply.github.com>
2019-06-04 10:11:01 +0900

    변경자 : 조용환 변경사항 : revauction DB저장 진행중

--- DESIGNUS/src/main/java/com/designus/www/AuctionController_sub.java
+++ DESIGNUS/src/main/java/com/designus/www/AuctionController_sub.java
@@ -10,29 +10,27 @@ import org.springframework.web.servlet.ModelAndView;
 import com.designus.www.service.RevAuctionMM;
 
 @Controller
-public class AuctionController_sub {	
-		/*
-		 * @Autowired private CommonMM cm;
-		 */
-		
-		ModelAndView mav;
-		
-	/* @Autowired */
-	/* private RevAuctionMM ram; */
-		
-		
-		@RequestMapping(value = "/revauctionWrite", method = RequestMethod.GET)
-		public ModelAndView revAuctionWrite() {
-			mav = new ModelAndView();
-			mav.setViewName("revAuctionWrite"); // joinFrm 하나 생성
-			return mav;
-		}
-		
-		@RequestMapping(value = "/revauctionsubmit", method = RequestMethod.POST)
-		public ModelAndView revAuctionSubmit(MultipartHttpServletRequest multi) {
-			mav = new ModelAndView();
-		/* mav = ram.revAuctionSubmit(multi); */
-			return mav;
-		}
-		
+public class AuctionController_sub {
+	/*
+	 * @Autowired private CommonMM cm;
+	 */
+
+	ModelAndView mav;
+
+	@Autowired
+	private RevAuctionMM ram;
+
+	@RequestMapping(value = "/revauctionWrite", method = RequestMethod.GET)
+	public ModelAndView revAuctionWrite() {
+		mav = new ModelAndView();
+		mav.setViewName("revAuctionWrite"); // joinFrm 하나 생성
+		return mav;
 	}
+
+	@RequestMapping(value = "/revauctionsubmit", method = RequestMethod.POST)
+	public ModelAndView revAuctionSubmit(MultipartHttpServletRequest multi) {
+		mav = new ModelAndView();
+		mav = ram.revAuctionSubmit(multi);
+		return mav;
+	}
+}
--- DESIGNUS/src/main/java/com/designus/www/dao/IRevAuctionDao.java
+++ DESIGNUS/src/main/java/com/designus/www/dao/IRevAuctionDao.java
@@ -5,10 +5,14 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
 
+import com.designus.www.bean.RevAuction;
+
 //import org.springframework.stereotype.Repository;
 
 //@Repository
 public interface IRevAuctionDao {
+
+	boolean revAuctionSubmitInsert(RevAuction ra);
   
   
 }
