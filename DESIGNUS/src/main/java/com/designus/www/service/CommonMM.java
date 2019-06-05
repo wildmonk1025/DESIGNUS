fc5e587f8aa1ab0e1f6e0418abb89e70870eca96
정병남 <qudskazoq@gmail.com>
2019-06-05 11:15:16 +0900

    정병남 형준이꺼 커밋하기위해

--- DESIGNUS/src/main/java/com/designus/www/service/CommonMM.java
+++ DESIGNUS/src/main/java/com/designus/www/service/CommonMM.java
@@ -1,11 +1,24 @@
 package com.designus.www.service;
 
+import javax.servlet.http.HttpSession; 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.designus.www.bean.Auction;
 import com.designus.www.bean.Member;
+import com.designus.www.dao.IcommonDao;
 
 public class CommonMM {
+	@Autowired
+	private IcommonDao cDao;
+	@Autowired
+	private HttpSession session;
+	private ModelAndView mav;
 
-
-
+	public ModelAndView bestajax(Auction at) {
+		mav =  new ModelAndView();
+		at = cDao.getbestInfo(at.getAu_mbid_w());
+		return mav;
+	}
 }
