a1d78d80541375179e708ba715508267d6e65c23
정병남 <qudskazoq@gmail.com>
2019-06-07 17:54:22 +0900

    정병남 아이디 비번찾기 진행중

--- DESIGNUS/src/main/java/com/designus/www/MailController.java
+++ DESIGNUS/src/main/java/com/designus/www/MailController.java
@@ -1,47 +1,109 @@
 package com.designus.www;
 
+import java.util.Random;
 import java.util.logging.Logger;
 
 import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.designus.www.bean.Member;
+import com.designus.www.email.MailService;
 import com.designus.www.service.MemberMM;
 import com.designus.www.service.ServiceMM;
-
-/*@Controller
+@Repository 
+@Controller
 public class MailController {
-	@RequestMapping(value = "emailConfirm", method = RequestMethod.GET)
-	public String mailSending(HttpServletRequest request) {
-		String setfrom = "아이디@gmail.com";
-		String tomail = request.getParameter("tomail"); // 받는 사람 이메일
-		String title = request.getParameter("title"); // 제목
-		String content = request.getParameter("content"); // 내용
-		try {
-			MimeMessage message = mailSender.createMimeMessage();
-			MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+    private UserService userService;
+    private MailService mailService;
+    Member mb;
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+ 
+    public void setMailService(MailService mailService) {
+        this.mailService = mailService;
+    }
+ 
+    // 회원가입 이메일 인증
+    @RequestMapping(value = "/sendMailauth", method = RequestMethod.POST, produces = "application/json")
+    @ResponseBody
+    public boolean sendMailAuth(HttpSession session, @RequestParam String email) {
+        int ran = new Random().nextInt(100000) + 10000; // 10000 ~ 99999
+        String joinCode = String.valueOf(ran);
+        session.setAttribute("joinCode", joinCode);
+ 
+        String subject = "회원가입 인증 코드 발급 안내 입니다.";
+        StringBuilder sb = new StringBuilder();
+        sb.append("귀하의 인증 코드는 " + joinCode + " 입니다.");
+        return mailService.send(subject, sb.toString(), "아이디@gmail.com", email, null);
+    }
+ 
+    // 아이디 찾기
+    @RequestMapping(value = "/sendMailid", method = RequestMethod.POST)
+    public String sendMailId(HttpSession session, @RequestParam String email, @RequestParam String captcha, RedirectAttributes ra) {
+        String captchaValue = (String) session.getAttribute("captcha");
+        if (captchaValue == null || !captchaValue.equals(captcha)) {
+            ra.addFlashAttribute("resultMsg", "자동 방지 코드가 일치하지 않습니다.");
+            return "redirect:/find/id";
+        }
+ 
+        User user = userService.findAccount(email);
+        if (user != null) {
+            String subject = "아이디 찾기 안내 입니다.";
+            StringBuilder sb = new StringBuilder();
+            sb.append("귀하의 아이디는 " + mb.getMb_id() + " 입니다.");
+            mailService.send(subject, sb.toString(), "아이디@gmail.com", email, null);
+            ra.addFlashAttribute("resultMsg", "귀하의 이메일 주소로 해당 이메일로 가입된 아이디를 발송 하였습니다.");
+        } else {
+            ra.addFlashAttribute("resultMsg", "귀하의 이메일로 가입된 아이디가 존재하지 않습니다.");
+        }
+        return "redirect:/find/id";
+    }
+ 
+    // 비밀번호 찾기
+    @RequestMapping(value = "/sendMailpassword", method = RequestMethod.POST)
+    public String sendMailPassword(HttpSession session, @RequestParam String id, @RequestParam String email, @RequestParam String captcha, RedirectAttributes ra) {
+        String captchaValue = (String) session.getAttribute("captcha");
+        if (captchaValue == null || !captchaValue.equals(captcha)) {
+            ra.addFlashAttribute("resultMsg", "자동 방지 코드가 일치하지 않습니다.");
+            return "redirect:/find/password";
+        }
+ 
+        User user = userService.findAccount(email);
+        if (user != null) {
+            if (!mb.getMb_id().equals(id)) {
+                ra.addFlashAttribute("resultMsg", "입력하신 이메일의 회원정보와 가입된 아이디가 일치하지 않습니다.");
+                return "redirect:/find/password";
+            }
+            int ran = new Random().nextInt(100000) + 10000; // 10000 ~ 99999
+            String password = String.valueOf(ran);
+            userService.updateInfo(mb.getMb_pw(), "password", password); // 해당 유저의 DB정보 변경
+ 
+            String subject = "임시 비밀번호 발급 안내 입니다.";
+            StringBuilder sb = new StringBuilder();
+            sb.append("귀하의 임시 비밀번호는 " + password + " 입니다.");
+            mailService.send(subject, sb.toString(), "아이디@gmail.com", email, null);
+            ra.addFlashAttribute("resultMsg", "귀하의 이메일 주소로 새로운 임시 비밀번호를 발송 하였습니다.");
+        } else {
+            ra.addFlashAttribute("resultMsg", "귀하의 이메일로 가입된 아이디가 존재하지 않습니다.");
+        }
+        return "redirect:/find/password";
+    }
 
-			messageHelper.setFrom(setfrom); // 보내는사람 생략하거나 하면 정상작동을 안함
-			messageHelper.setTo(tomail); // 받는사람 이메일
-			messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
-			messageHelper.setText(content); // 메일 내용
-
-			mailSender.send(message);
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		return "emailConfirm";
-	}
-*/
-
+}
