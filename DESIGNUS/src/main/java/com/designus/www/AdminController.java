5081174a580f97c1026adb5038c5aff05b257d8b
poiuaa11 <pain_miso@naver.com>
2019-06-12 09:31:35 +0900

    어드민

--- DESIGNUS/src/main/java/com/designus/www/AdminController.java
+++ DESIGNUS/src/main/java/com/designus/www/AdminController.java
@@ -2,12 +2,13 @@ package com.designus.www;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminController {
-	/*
-	 * @RequestMapping()
-	 * public String norJoinFrm() { return "norjoinFrm"; }
-	 */
-	 
+	@RequestMapping(value = "/admininfo", method = RequestMethod.GET)
+	public String admininfo() {
+
+		return "admininfo";
+	}
 }
