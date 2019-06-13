c68aac0f88fe725f5320b9c318677bb05a7b43b7
volvohi1 <volvohi1@naver.com>
2019-06-13 09:01:06 +0900

    소형준 auction 끝!!?

--- DESIGNUS/src/main/java/com/designus/www/AdminController_SO.java
+++ DESIGNUS/src/main/java/com/designus/www/AdminController_SO.java
@@ -6,34 +6,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminController_SO {
-	@RequestMapping(value = "/adminInfo", method = RequestMethod.GET)
-	public String admininfo() {
-
-		return "adminInfo";
-	}
-	@RequestMapping(value = "/declareWrite", method = RequestMethod.GET)
-	public String declareWrite() {
-		
-		return "declareWrite";
-	}
-	@RequestMapping(value = "/questionList", method = RequestMethod.GET)
-	public String questionList() {
-		
-		return "questionList";
-	}
-	@RequestMapping(value = "/memberList", method = RequestMethod.GET)
-	public String memberList() {
-		
-		return "memberList";
-	}
+	
 	@RequestMapping(value = "/permitWriApply", method = RequestMethod.GET)
 	public String permitWriApply() {
 		
 		return "permitWriApply";
 	}
-	@RequestMapping(value = "/categoryEdit", method = RequestMethod.GET)
-	public String categoryEdit() {
-		
-		return "categoryEdit";
-	}
+	
 }
