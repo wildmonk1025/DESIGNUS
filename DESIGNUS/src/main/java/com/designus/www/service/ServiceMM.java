be9ae6845b51cd938a36e098838c485a9877c471
volvohi1 <volvohi1@naver.com>
2019-06-13 17:08:52 +0900

    소형준 커밋

--- DESIGNUS/src/main/java/com/designus/www/service/ServiceMM.java
+++ DESIGNUS/src/main/java/com/designus/www/service/ServiceMM.java
@@ -1,7 +1,21 @@
 package com.designus.www.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 
+@Service
 public class ServiceMM {
+
+	private ModelAndView mav;	
+	
+	public ModelAndView servicescquestion() {
+		mav = new ModelAndView();
+		String view ="sc_Question";
+		
+		
+		mav.setViewName(view);
+		return mav;
+	}
 	
 	
 	
