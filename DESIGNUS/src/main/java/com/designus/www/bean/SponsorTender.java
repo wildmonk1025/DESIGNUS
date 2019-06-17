f0d7278233792246dd0078329020070835e21eda
poiuaa11 <pain_miso@naver.com>
2019-06-17 11:35:37 +0900

    ㅇ<-<

--- DESIGNUS/src/main/java/com/designus/www/bean/SponsorTender.java
+++ DESIGNUS/src/main/java/com/designus/www/bean/SponsorTender.java
@@ -1,6 +1,14 @@
 package com.designus.www.bean;
 
+import org.apache.ibatis.type.Alias;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Alias("sponsortender")
+@Getter
+@Setter
 public class SponsorTender {
-	int    sst_ssnum;
+	int sst_ssnum;
 	String sst_mbid;
 }
