872f905b683699ea899b07e6f8a0a08a36d28d4d
volvohi1 <volvohi1@naver.com>
2019-06-17 10:51:22 +0900

    소형준 AuctionTender notyfy 하는중

--- DESIGNUS/src/main/java/com/designus/www/service/AuctionMM.java
+++ DESIGNUS/src/main/java/com/designus/www/service/AuctionMM.java
@@ -199,11 +199,13 @@ public class AuctionMM {
 		at.setAut_price(totalPrice);
 		
 		if(Tqty > 0) {
-			
-			
 			aDao.setAuctionTenderDel(at);
 			aDao.setAuctionTenderI(at);
 			aDao.setAuctionUTI(at);
+
+			at.setAut_mbid(aDao.getAuctionWID(at));
+			at.setAu_title(aDao.getAuctionTitle(at));
+			at.setAut_notify(id+" 님이 " + at.getAu_title() + " 상품을" + at.getAut_qty() +" 개 구입 하셧습니다.");
 			aDao.setNotifyAuctionTender(at);
 			
 		}
