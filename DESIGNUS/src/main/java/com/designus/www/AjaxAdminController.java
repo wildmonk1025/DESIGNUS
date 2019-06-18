af618c1cca998e85081b16a7d848b44f348bf42d
wildmonk1025 <50566047+wildmonk1025@users.noreply.github.com>
2019-06-18 10:06:45 +0900

    zxczxc

--- DESIGNUS/src/main/java/com/designus/www/AjaxAdminController.java
+++ DESIGNUS/src/main/java/com/designus/www/AjaxAdminController.java
@@ -1,7 +1,6 @@
 package com.designus.www;
 
-import org.springframework.beans.factory.annotation.Autowired;  
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,42 +11,49 @@ import com.designus.www.service.AdminMM;
 @RestController
 public class AjaxAdminController {
 	@Autowired
-	private AdminMM am;
-	  @RequestMapping() public String norJoinFrm() {
-	  return "norjoinFrm";
-	  }
-		@RequestMapping(value = "/declarewritecheck", method = RequestMethod.POST, produces="application/json; charset=utf-8;")
-		public String declarewritecheck() {
-			String check = am.declarewritecheck();
-			 System.out.println("cm="+am.declarewritecheck());
-			  System.out.println("aList="+check);
-			return check;
-			//관리자페이지 신고하기 정보
-		}
-		@RequestMapping(value = "/Declarelist", method = RequestMethod.POST, produces="application/json; charset=utf-8;")
-		public String Declarelist() {
-			String check = am.Declarelist();
-			return check;
-			//신고내역페이지 신고정보
-		}
-		@RequestMapping(value = "/transformList", method = RequestMethod.POST, produces="application/json; charset=utf-8;")
-		public String transformList() {
-			String check = am.transformList();
-			return check;
-			//관리자페이지 작가신청정보
-		}
-		@RequestMapping(value = "/transformwridetail", method = RequestMethod.POST, produces="application/json; charset=utf-8;")
-		public String transformwridetail() {
-			String check = am.transformwridetail();
-			System.out.println("aList="+check);
-			return check;
-			//작가전환페이지 신청정보
-		}
-		@RequestMapping(value = "/questionWrite", method = RequestMethod.POST, produces="application/json; charset=utf-8;")
-		public @ResponseBody String questionwritecheck() {
-			String check = am.questionwritecheck();
-			return check;
-			
-			//1:1문의 리스트
-}
+	private AdminMM adm;
+
+	@RequestMapping()
+	public String norJoinFrm() {
+		return "norjoinFrm";
+	}
+
+	@RequestMapping(value = "/declarewritecheck", method = RequestMethod.POST, produces = "application/json; charset=utf-8;")
+	public String declarewritecheck() {
+		String check = adm.declarewritecheck();
+		System.out.println("cm=" + adm.declarewritecheck());
+		System.out.println("aList=" + check);
+		return check;
+		//관리자페이지 신고하기 정보
+	}
+
+	@RequestMapping(value = "/Declarelist", method = RequestMethod.POST, produces = "application/json; charset=utf-8;")
+	public String Declarelist() {
+		String check = adm.Declarelist();
+		return check;
+		//신고내역페이지 신고정보
+	}
+
+	@RequestMapping(value = "/transformList", method = RequestMethod.POST, produces = "application/json; charset=utf-8;")
+	public String transformList() {
+		String check = adm.transformList();
+		return check;
+		//관리자페이지 작가신청정보
+	}
+
+	@RequestMapping(value = "/transformwridetail", method = RequestMethod.POST, produces = "application/json; charset=utf-8;")
+	public String transformwridetail() {
+		String check = adm.transformwridetail();
+		System.out.println("aList=" + check);
+		return check;
+		//작가전환페이지 신청정보
+	}
+
+	@RequestMapping(value = "/questionWrite", method = RequestMethod.POST, produces = "application/json; charset=utf-8;")
+	public @ResponseBody String questionwritecheck() {
+		String check = adm.questionwritecheck();
+		return check;
+
+		//1:1문의 리스트
+	}
 }
--- DESIGNUS/src/main/java/com/designus/www/dao/IadminDao.java
+++ DESIGNUS/src/main/java/com/designus/www/dao/IadminDao.java
@@ -44,11 +44,5 @@ public interface IadminDao {
 	Question getquestionCheck(@Param("aq_num") int aq_num);
 
 	QuestionReply getquestionReply(Map<String,Object> map);
-
-
-	
-	
-	
-  
   
 }
