58ca7046ae5eee9038671f31cda30bd58226b571
poiuaa11 <pain_miso@naver.com>
2019-06-21 17:28:41 +0900

    제어,,

--- DESIGNUS/src/main/java/com/designus/www/service/SponsorMM.java
+++ DESIGNUS/src/main/java/com/designus/www/service/SponsorMM.java
@@ -111,6 +111,32 @@ public class SponsorMM {
 		System.out.println("넘어오나용" + ss_num);
 		sm = sDao.getSponcheck(ss_num);
 		sp = sDao.getCountcheck(ss_num);
+		
+		String date1 = sm.getSs_date();
+		String date2 = sm.getEnd_data2();
+		
+		 try{ // String Type을 Date Type으로 캐스팅하면서 생기는 예외로 인해 여기서 예외처리 해주지 않으면 컴파일러에서 에러가 발생해서 컴파일을 할 수 없다.
+		        SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+		        // date1, date2 두 날짜를 parse()를 통해 Date형으로 변환.
+		        Date FirstDate = format.parse(date1);
+		        Date SecondDate = format.parse(date2);
+		        
+		        // Date로 변환된 두 날짜를 계산한 뒤 그 리턴값으로 long type 변수를 초기화 하고 있다.
+		        // 연산결과 -950400000. long type 으로 return 된다.
+		        long calDate = FirstDate.getTime() - SecondDate.getTime(); 
+		        
+		        // Date.getTime() 은 해당날짜를 기준으로1970년 00:00:00 부터 몇 초가 흘렀는지를 반환해준다. 
+		        // 이제 24*60*60*1000(각 시간값에 따른 차이점) 을 나눠주면 일수가 나온다.
+		        long calDateDays = calDate / ( 24*60*60*1000); 
+		 
+		        calDateDays = Math.abs(calDateDays);
+		        
+		        System.out.println("두 날짜의 날짜 차이: "+calDateDays);
+		        }
+		        catch(ParseException e)
+		        {
+		            // 예외 처리
+		        }
 
 		System.out.println("담기나??=" + sp.getSsp_count());
 
@@ -142,14 +168,22 @@ public class SponsorMM {
 
 	public String sponbuy(Sponsor ss) {
 
+		Sponsor sm = new Sponsor();
+		SponsorProgress sp = new SponsorProgress();
+
 		System.out.println("후원 밀어주기??");
 
 		String id = (String) session.getAttribute("id");
 		String grade = (String) session.getAttribute("grade");
 
-		ss.getSs_num();
+		int ss_num = ss.getSs_num();
 		ss.setSs_mbid_w(id);
 
+		System.out.println(ss_num);
+		
+		sm = sDao.getSponcheck(ss_num);
+		sp = sDao.getCountcheck(ss_num);
+		
 		String json = new Gson().toJson(ss);
 
 		System.out.println("회원등급=" + grade);
